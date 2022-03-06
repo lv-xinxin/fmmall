8298fb460ae7b8a7e57108d36dc84f223f6bcde6
lvxinxin <lxx1lxx@126.com>
2022-03-06 22:53:33 +0800

    开始2

--- api/src/main/java/com/qfedu/fmmall/ApiApplication.java
+++ api/src/main/java/com/qfedu/fmmall/ApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("dao")
+@MapperScan("com.qfedu.fmmall.dao")
 public class ApiApplication {
 
     public static void main(String[] args) {
---
+++ common/src/main/java/com/qfedu/fmmall/vo/ResultVO.java
@@ -0,0 +1,22 @@
+package com.qfedu.fmmall.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResultVO {
+
+    //响应前端状态码
+    private int code;
+    //响应前端提示信息
+    private String msg;
+    //响应前端数据
+    private String data;
+
+
+
+
+}
