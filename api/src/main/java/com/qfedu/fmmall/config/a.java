4d83ea063a925af4b2f624ac45cb62968eebfd02
zhosq <435221087@qq.com>
2022-04-01 15:58:53 +0800

    aa

--- api/src/main/java/com/qfedu/fmmall/config/a.java
+++ api/src/main/java/com/qfedu/fmmall/config/a.java
@@ -2,7 +2,8 @@ package com.qfedu.fmmall.config;
 
 public class a {
      public static void main(String[] args) {
-        System.out.println("1123123123123123123123123");
+        System.out.println("123564879吕鑫鑫");
+
 
     }
 
