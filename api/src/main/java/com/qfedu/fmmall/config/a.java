381fb3978208bfd152023d15b657ec9663500244
zhosq <435221087@qq.com>
2022-04-01 15:48:25 +0800

    11

--- api/src/main/java/com/qfedu/fmmall/config/a.java
+++ api/src/main/java/com/qfedu/fmmall/config/a.java
@@ -1,5 +1,34 @@
 package com.qfedu.fmmall.config;
 
 public class a {
+111    public static void main(String[] args) {
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+        System.out.println("1123");
+
+    }
 
 }
